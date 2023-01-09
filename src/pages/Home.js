2e1614456c0317b0ef4c9881d9b1db1cc12ed82b
kajal-2011 <kajalrekha888@gmail.com>
2023-01-10 00:23:06 +0600

    header markup done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,11 @@
+import Header from "../components/Header";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
